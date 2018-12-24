ea4497a131a9cd494617d85221a0882b700e3b4c
jancee <jancee.wang@qq.com>
2018-12-24 18:00:48 +0800

    update docker build script

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -3,6 +3,6 @@
 docker build . -t jancee/update-server:latest
 docker tag jancee/update-server:latest seeed.azurecr.io/seeed-update-image-generator:1.0
 docker tag jancee/update-server:latest seeed.azurecr.io/seeed-update-image-generator:latest
-
-
+docker push seeed.azurecr.io/seeed-update-image-generator:1.0
+docker push seeed.azurecr.io/seeed-update-image-generator:latest
 
