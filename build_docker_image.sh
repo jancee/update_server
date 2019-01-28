5a78ff5520ef76394c7e4dbe50cf42957e3cb92f
jancee <jancee.wang@qq.com>
2019-01-28 15:04:27 +0800

    feat(output): append "signed_metadata_hash" to "update.meta" file

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 docker build . -t jancee/update-server:latest
-docker tag jancee/update-server:latest seeed.azurecr.io/seeed-update-image-generator:1.1
+
+docker tag jancee/update-server:latest jancee/seeed-update-image-generator:1.2
+docker tag jancee/update-server:latest jancee/seeed-update-image-generator:latest
+docker tag jancee/update-server:latest seeed.azurecr.io/seeed-update-image-generator:1.2
 docker tag jancee/update-server:latest seeed.azurecr.io/seeed-update-image-generator:latest
-docker push seeed.azurecr.io/seeed-update-image-generator:1.1
+
+docker push jancee/seeed-update-image-generator:1.2
+docker push jancee/seeed-update-image-generator:latest
+docker push seeed.azurecr.io/seeed-update-image-generator:1.2
 docker push seeed.azurecr.io/seeed-update-image-generator:latest
 
